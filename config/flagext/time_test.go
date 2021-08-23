6f8b12104c744b79c66680bea286c9eea68b9564
Ji-Ping Shen <5998992+jiping-s@users.noreply.github.com>
2021-08-23 11:36:31 +0300

    Support date parsing in config/structargs (#12)

--- config/flagext/time_test.go
+++ config/flagext/time_test.go
@@ -18,3 +18,16 @@ func TestTimeVar(t *testing.T) {
 	assert.Nil(t, f.Parse([]string{"--start", "2021-01-02T03:04:05.567Z"}))
 	assert.Equal(t, "2021-01-02T03:04:05.567Z", tm.Format(time.RFC3339Nano))
 }
+
+func TestTimeParse(t *testing.T) {
+	{
+		var tm timeValue
+		assert.Nil(t, tm.Set("2020-12-31"))
+		assert.Equal(t, time.Time(tm).Format(time.RFC3339Nano), time.Date(2020, 12, 31, 0, 0, 0, 0, time.Local).Format(time.RFC3339Nano))
+	}
+	{
+		var tm timeValue
+		assert.Nil(t, tm.Set("2020-12-31T10:30:15"))
+		assert.Equal(t, time.Time(tm).Format(time.RFC3339Nano), time.Date(2020, 12, 31, 10, 30, 15, 0, time.Local).Format(time.RFC3339Nano))
+	}
+}
