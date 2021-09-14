5ea8e05154c2bcd035958e08d5bce32d67c71e1d
Ji-Ping Shen <ji-ping.shen@relexsolutions.com>
2021-09-14 16:11:15 +0300

    Fix comment

--- promexporter/promext/errors.go
+++ promexporter/promext/errors.go
@@ -1,4 +1,5 @@
 // Copyright 2021 RELEX Oy
+// Copyright 2014 The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,7 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package promext contains custom metric types and pure utility functions
-//
-// Types and functions inside are at the lowest level and should not call Logger or other packages, to avoid cyclic dependency
 package promext
+
+import (
+	"fmt"
+)
+
+func makeInconsistentCardinalityError(fqName string, labels, labelValues []string) error {
+	return fmt.Errorf(
+		"inconsistent label cardinality: %q has %d variable labels named %q but %d values %q were provided",
+		fqName,
+		len(labels), labels,
+		len(labelValues), labelValues,
+	)
+}
