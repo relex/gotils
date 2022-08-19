593c7f87b59eeffd944e52c2e7bedff1da64710f
Ji-Ping Shen <5998992+jiping-s@users.noreply.github.com>
2022-08-19 08:40:48 +0300

    fix: Allow overriding of make commands, add default build (#29)

--- Common.mk
+++ Common.mk
@@ -12,47 +12,58 @@
 # limitations under the License.
 
 SHELL := /bin/bash
+
+AUTO_BUILD_VERSION ?= dev
+OURDIR := BUILD
+OUTPUT := ${OURDIR}/$(shell basename $(shell pwd))
+
 SOURCES := $(shell find . -name '*.go')
 SOURCES_NONTEST := $(shell find . -name '*.go' -not -name '*_test.go')
 
-# .PHONY: build
-# build:
-# 	CGO_ENABLED=$${CGO_ENABLED:-0} GO_LDFLAGS="-X main.version=$(AUTO_BUILD_VERSION)" go build
+.PHONY: build-default
+build-default: ${OUTPUT}
 
-# test is the first/default target for "make" if parent project doesn't define any
-.PHONY: test
-test:
+# use pattern % to prevent warning when overridden in parent Makefile
+${OUTPUT}%: Makefile go.mod $(SOURCES_NONTEST)
+	CGO_ENABLED=$${CGO_ENABLED:-0} GO_LDFLAGS="-X main.version=$(AUTO_BUILD_VERSION)" go build -o ${OUTPUT}
+
+.PHONY: test-default
+test-default:
 	CGO_ENABLED=$${CGO_ENABLED:-0} LOG_LEVEL=$${LOG_LEVEL:-warn} LOG_COLOR=$${LOG_COLOR:-Y} go test -timeout $${TEST_TIMEOUT:-10s} -v ./...
 
 # test-all ignores testcache (go clean testcache)
-.PHONY: test-all
-test-all:
+.PHONY: test-all-default
+test-all-default:
 	CGO_ENABLED=$${CGO_ENABLED:-0} LOG_LEVEL=$${LOG_LEVEL:-warn} LOG_COLOR=$${LOG_COLOR:-Y} go test -timeout $${TEST_TIMEOUT:-10s} -v -count=1 ./...
 
-# the rest of targets below are sorted by workflow
+# The rest of targets should be sorted by workflow and frequency
 
-.PHONY: prereq
-prereq:
+.PHONY: prereq-default
+prereq-default:
 	gotils-prereq.sh
 
-.PHONY: gen
-gen:
+.PHONY: gen-default
+gen-default:
 	go generate ./...
 
-.PHONY: pretty
-pretty:
+.PHONY: pretty-default
+pretty-default:
 	gotils-lint.sh --fix
 
-.PHONY: lint
-lint:
+.PHONY: lint-default
+lint-default:
 	gotils-lint.sh
 
-.PHONY: clean
-clean:
+.PHONY: clean-default
+clean-default:
 	rm -rf BUILD/*
 	go clean
 
-.PHONY: upgrade
-upgrade:
+.PHONY: upgrade-default
+upgrade-default:
 	rm -f go.sum
 	go get -u -d ./...; go mod tidy
+
+# Redirect commands to the default ones if not provided by the parent Makefile
+%: %-default
+	@true
